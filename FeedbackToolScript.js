b53480ba7f7fe31ae386232a1fdc69473b878cd0
Hamza Aburaneh <Hamza.Aburaneh@carleton.ca>
2021-10-05 13:57:23 -0400

    Revert "Update FeedbackToolScript.js"
    This reverts commit fb2789155b1bd9ce81b1495b22406994a3970ded.

--- FeedbackToolScript.js
+++ FeedbackToolScript.js
@@ -50,7 +50,14 @@
             });
         });
     });
-
+    $document.on("keyup", selector + " textarea", function(event) {
+        var textValue = event.target.value;
+        if (textValue.match(regex.phoneNumber) || textValue.match(regex.sin) || textValue.match(regex.postalCode) || textValue.match(regex.email)) {
+            $("#warning").removeClass("hidden").animate();
+        } else {
+            $("#warning").addClass("hidden");
+        }
+    });
 
     $document.on("timerpoke.wb " + initEvent, selector, init);
     wb.add(selector);

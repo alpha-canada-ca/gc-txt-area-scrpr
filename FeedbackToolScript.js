e3ec95d576d9813330e2c006ef9e1fe554214a60
Hamza Aburaneh <Hamza.Aburaneh@carleton.ca>
2021-10-05 13:35:03 -0400

    Update FeedbackToolScript.js

--- FeedbackToolScript.js
+++ FeedbackToolScript.js
@@ -28,7 +28,28 @@
         }, "Please remove your SIN number.");
     }, 2000);
 
-
+    $(document).on("wb-ready.wb", function() {
+        $("#btnno").click(function(e) {
+            $(".gc-pg-hlpfl-no").removeClass("nojs-show");
+            $(".gc-pg-hlpfl-btn").addClass("hide");
+            $("#helpful").val("No");
+        });
+        $("#gc-pg-hlpfl-frm").submit(function(e) {
+            e.preventDefault();
+            $(".gc-pg-hlpfl-thnk").removeClass("hide");
+            $("#gc-pg-hlpfl-frm").addClass("hide nojs-show");
+            $.ajax({
+                url: "https://pagesuccessemailqueue.azurewebsites.net/api/QueueProblemForm",
+                type: "POST",
+                dataType: "text",
+                data: $("form#gc-pg-hlpfl-frm").serialize(),
+                success: function(data) {},
+                error: function(xhr, status, err) {
+                    console.log(xhr.responseText);
+                },
+            });
+        });
+    });
     $document.on("keyup", selector + " textarea", function(event) {
         var textValue = event.target.value;
         if (textValue.match(regex.phoneNumber) || textValue.match(regex.sin) || textValue.match(regex.postalCode) || textValue.match(regex.email)) {

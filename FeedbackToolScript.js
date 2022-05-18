6bb5113c58e1b47816045f78a05e5f5a91856dce
Hamza Aburaneh <Hamza.Aburaneh@carleton.ca>
2022-05-18 11:55:27 -0400

    Update FeedbackToolScript.js

--- FeedbackToolScript.js
+++ FeedbackToolScript.js
@@ -1,75 +1,39 @@
-(function($, windows, wb) {
-    
-    const getDeviceType = () => {
-      const ua = navigator.userAgent;
-      if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-        return "tablet";
-      }
-      if (
-        /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
-          ua
-        )
-      ) {
-        return "mobile";
-      }
-      return "desktop";
-    };
-    
-    $('#deviceType').val(getDeviceType());
 
+const getDeviceType = () => {
+  const ua = navigator.userAgent;
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+    return "tablet";
+  }
+  if (
+    /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+      ua
+    )
+  ) {
+    return "mobile";
+  }
+  return "desktop";
+};
 
-    "use strict";
-    var componentName = "gc-pg-hlpfl",
-        selector = "." + componentName,
-        initEvent = "wb-init" + selector,
-        $document = wb.doc,
-        regex = {
-            phoneNumber: /(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}/im,
-            sin: /(\d{3}\s*\d{3}\s*\d{3}|\d{3}\D*\d{3}\D*\d{3})/im,
-            postalCode: /[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d/im,
-            email: /([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})/im
-        },
-        init = function(event) {
-            var elm = wb.init(event, componentName, selector),
-                $elm;
-            if (elm) {
-                $elm = $(elm);
-                $elm.trigger("keyup");
-                wb.ready($elm, componentName);
-            }
-        };
-
-
-    setTimeout(function() {
-        $.validator.addMethod("sinca", function(sin_number, element) {
-            return !(/(\d{3}\s*\d{3}\s*\d{3}|\d{3}\D*\d{3}\D*\d{3})/.test(sin_number))
-        }, "Please remove your SIN number.");
-    }, 4000);
-
-    $(document).on("wb-ready.wb", function() {
-        $("#btnno").click(function(e) {
-            $(".gc-pg-hlpfl-no").removeClass("nojs-show");
-            $(".gc-pg-hlpfl-btn").addClass("hide");
-            $("#helpful").val("No");
-        });
-        // $("#gc-pg-hlpfl-frm").submit(function(e) {
-        //     e.preventDefault();
-        //     $(".gc-pg-hlpfl-thnk").removeClass("hide");
-        //     $("#gc-pg-hlpfl-frm").addClass("hide nojs-show");
-        //     $.ajax({
-        //         url: "https://pagesuccessemailqueue.azurewebsites.net/api/QueueProblemForm",
-        //         type: "POST",
-        //         dataType: "text",
-        //         data: $("form#gc-pg-hlpfl-frm").serialize(),
-        //         success: function(data) {},
-        //         error: function(xhr, status, err) {
-        //             console.log(xhr.responseText);
-        //         },
-        //     });
-        // });
+$(document).on("wb-ready.wb", function () {
+  $("#btnno").click(function (e) {
+    $(".gc-pg-hlpfl-no").removeClass("nojs-show");
+    $(".gc-pg-hlpfl-btn").addClass("hide");
+    $("#helpful").val("No");
+    $("#deviceTyoe").val(getDeviceType());
+  });
+  $("#gc-pg-hlpfl-frm").submit(function (e) {
+    e.preventDefault();
+    $(".gc-pg-hlpfl-thnk").removeClass("hide");
+    $("#gc-pg-hlpfl-frm").addClass("hide nojs-show");
+    $.ajax({
+      url: "https://pagesuccessemailqueue.azurewebsites.net/api/QueueProblemForm",
+      type: "POST",
+      dataType: "text",
+      data: $("form#gc-pg-hlpfl-frm").serialize(),
+      success: function (data) {},
+      error: function (xhr, status, err) {
+        console.log(xhr.responseText);
+      },
     });
-
-
-    $document.on("timerpoke.wb " + initEvent, selector, init);
-    wb.add(selector);
-})(jQuery, window, wb);
+  });
+});
